ecaaa98d8b953088ad671d10d0b1ba54bc2e3b78
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-08-02 21:32:33 -0300

    trying to make ChatJS work with RequireJS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 require.config({
 	paths: {
 		jquery: '../bower_components/jquery/jquery',
-		bootstrap: 'vendor/bootstrap'
+		bootstrap: 'vendor/bootstrap',
+		chart: 'vendor/chart'
 	},
 	shim: {
 		bootstrap: {
@@ -11,17 +12,23 @@ require.config({
 	}
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
+require(['app', 'jquery', 'bootstrap', 'chart'], function (app, $, chart) {
 	'use strict';
 	// use app here    
 	//console.log(app);
 	//console.log('Running jQuery %s', $().jquery);
-	function soma() {
-		var totalV = 0;
-		var totalW = 0;
-		var totalX = 0;
-		var totalY = 0;
-		var totalZ = 0;
+	var totalV = 0;
+	var totalW = 0;
+	var totalX = 0;
+	var totalY = 0;
+	var totalZ = 0;
+
+	function countAnswers() {
+		totalV = 0;
+		totalW = 0;
+		totalX = 0;
+		totalY = 0;
+		totalZ = 0;
 		$('.VTotal').val(0);
 		$('.WTotal').val(0);
 		$('.XTotal').val(0);
@@ -42,13 +49,19 @@ require(['app', 'jquery', 'bootstrap'], function (app, $) {
 		$('.ZAnswer:checked').each(function(){
 			totalZ++;
 		});
-		$('.VTotal').val(totalV);
-		$('.WTotal').val(totalW);
-		$('.XTotal').val(totalX);
-		$('.YTotal').val(totalY);
-		$('.ZTotal').val(totalZ);
 	}
+
+	function plotGraph(V, W, X, Y, Z){
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var data = {
+            labels : ['Fisiológica','Segurança','Associação','Auto-Estima','Auto-Realização'],
+            datasets : [{fillColor : 'rgba(151,187,205,0.5)',strokeColor : 'rgba(151,187,205,1)',data : [V,W,X,Y,Z]}]
+        };
+        var myNewChart = new Chart(ctx).Bar(data);
+	}
+
 	$('#SubmitForm').click(function(){
-		soma();
+		countAnswers();
+		plotGraph(totalV, totalW, totalX, totalY, totalZ);
 	});
 });
